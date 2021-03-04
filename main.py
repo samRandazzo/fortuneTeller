9d8c61373fb270f5d60d7ec362abd5a6406fce66
Sam Randazzo <sam.randazzo@protonmail.com>
2021-03-04 21:32:27 +0000

    added quote api for testing

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import json
 client = discord.Client()
 
 def get_quote():
-  response = request.get("https://zenquotes.io/api/random")
-  json_data = json.loads(respons.text)
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quit)
+  return(quote)
 
 @client.event
 async def on_ready():
@@ -22,7 +22,8 @@ async def on_message(message):
     return
 
   if message.content.startswith('$hello'):
-    await message.channel.send('Meka Leka Hi Meka Hiney Ho!')
+    quote = get_quote()
+    await message.channel.send('Meka Leka Hi Meka Hiney Ho!' + quote)
 
 client.run(os.getenv('TOKEN'))
 
